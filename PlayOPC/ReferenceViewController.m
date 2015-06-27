8f664cb1a1daa77d4967c19374c1b53f4798b57a
Hiroki Ishiura <ishiura@ja2.so-net.ne.jp>
2015-06-27 12:39:24 +0900

    インターネットに接続しているときは、内蔵ドキュメントの外部リンクをSafariで表示できるようにした。

--- PlayOPC/ReferenceViewController.m
+++ PlayOPC/ReferenceViewController.m
@@ -10,8 +10,10 @@
 //
 
 #import "ReferenceViewController.h"
+#import "Reachability.h"
+#import "UIViewController+Alert.h"
 
-@interface ReferenceViewController ()
+@interface ReferenceViewController () <UIWebViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 
@@ -27,6 +29,9 @@
 	DEBUG_LOG(@"");
 	[super viewDidLoad];
 	
+	// ビューを初期化します。
+	self.webView.delegate = self;
+	
 	// リファレンスマニュアルを表示します。
 	NSString *htmlFilePath = [[NSBundle mainBundle] pathForResource:@"Reference" ofType:@"html"];
 	NSURL *htmlFileUrl = [NSURL fileURLWithPath:htmlFilePath];
@@ -40,6 +45,8 @@
 
 - (void)dealloc {
 	DEBUG_LOG(@"");
+	
+	self.webView.delegate = nil;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -48,6 +55,29 @@
 	
 	// ツールバーを非表示にします。
 	[self.navigationController setToolbarHidden:YES animated:animated];
+}
+
+#pragma mark -
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+	DEBUG_LOG(@"request=%@", request);
+
+	// 内蔵ドキュメントなら常に許可します。
+	if ([request.URL.scheme isEqualToString:@"file"]) {
+		return YES;
+	}
+	
+	// 外部コンテンツならインターネットに接続されているかを確認します。
+	Reachability *reachability = [Reachability reachabilityWithHostName:@"itunes.apple.com"];
+	NetworkStatus status = [reachability currentReachabilityStatus];
+	if (status == NotReachable) {
+		[self showAlertMessage:NSLocalizedString(@"The device does not connect with the Internet. If you want to open the site via Wi-Fi it is necessary to disconnect camera connection first.", nil) title:NSLocalizedString(@"Could not open link", nil)];
+		return NO;
+	}
+	
+	// このWebViewでは開かずに、代わりにWebブラウザで開きます。
+	[[UIApplication sharedApplication] openURL:request.URL];
+	return NO;
 }
 
 @end

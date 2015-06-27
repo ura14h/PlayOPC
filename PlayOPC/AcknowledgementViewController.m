74119da96416a837e55513a9977f63fdd7fa97ab
Hiroki Ishiura <ishiura@ja2.so-net.ne.jp>
2015-06-27 19:23:05 +0900

    インターネットに接続しているときは、内蔵ドキュメントの外部リンクをSafariで表示できるようにした。

--- PlayOPC/AcknowledgementViewController.m
+++ PlayOPC/AcknowledgementViewController.m
@@ -10,8 +10,10 @@
 //
 
 #import "AcknowledgementViewController.h"
+#import "Reachability.h"
+#import "UIViewController+Alert.h"
 
-@interface AcknowledgementViewController ()
+@interface AcknowledgementViewController () <UIWebViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 
@@ -27,6 +29,9 @@
 	DEBUG_LOG(@"");
 	[super viewDidLoad];
 
+	// ビューを初期化します。
+	self.webView.delegate = self;
+	
 	// 謝辞を表示します。
 	NSString *htmlFilePath = [[NSBundle mainBundle] pathForResource:@"Acknowledgement" ofType:@"html"];
 	NSURL *htmlFileUrl = [NSURL fileURLWithPath:htmlFilePath];
@@ -48,6 +53,29 @@
 	
 	// ツールバーを非表示にします。
 	[self.navigationController setToolbarHidden:YES animated:animated];
+}
+
+#pragma mark -
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+	DEBUG_LOG(@"request=%@", request);
+	
+	// 内蔵ドキュメントなら常に許可します。
+	if ([request.URL.scheme isEqualToString:@"file"]) {
+		return YES;
+	}
+	
+	// 外部コンテンツならインターネットに接続されているかを確認します。
+	Reachability *reachability = [Reachability reachabilityWithHostName:@"itunes.apple.com"];
+	NetworkStatus status = [reachability currentReachabilityStatus];
+	if (status == NotReachable) {
+		[self showAlertMessage:NSLocalizedString(@"The device does not connect with the Internet. If you want to open the site via Wi-Fi it is necessary to disconnect camera connection first.", nil) title:NSLocalizedString(@"Could not open link", nil)];
+		return NO;
+	}
+	
+	// このWebViewでは開かずに、代わりにWebブラウザで開きます。
+	[[UIApplication sharedApplication] openURL:request.URL];
+	return NO;
 }
 
 @end

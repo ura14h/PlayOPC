f1e35d9f790f086b7ea7e540ef69d357af0f003c
Hiroki Ishiura <ishiura@ja2.so-net.ne.jp>
2016-04-23 13:53:07 +0900

    Wi-Fiでカメラに接続できるかの確認の手法を変更した。
    カメラのCGIを直接呼び出す独自の手法から、ライブラリに追加された新しいメソッドを呼び出す手法に変更した。

--- PlayOPC/WifiConnector.m
+++ PlayOPC/WifiConnector.m
@@ -12,6 +12,8 @@
 #import "WifiConnector.h"
 #import <SystemConfiguration/CaptiveNetwork.h>
 #import "Reachability.h"
+#import "AppDelegate.h"
+#import "AppCamera.h"
 
 NSString *const WifiStatusChangedNotification = @"WifiStatusChangedNotification";
 
@@ -108,7 +110,7 @@ NSString *const WifiStatusChangedNotification = @"WifiStatusChangedNotification"
 
 	__weak WifiConnector *weakSelf = self;
 	dispatch_async(weakSelf.reachabilityQueue, ^{
-		[weakSelf updateStatusWithToPingCamera:YES];
+		[weakSelf updateStatusWithToKnockOnCamera:YES];
 		// 監視を開始します。
 		// MARK: メインスレッドで呼び出さないとコールバックが呼び出されないようです。
 		dispatch_async(dispatch_get_main_queue(), ^{
@@ -160,7 +162,7 @@ NSString *const WifiStatusChangedNotification = @"WifiStatusChangedNotification"
 	BOOL connected = NO;
 	NSDate *waitStartTime = [NSDate date];
 	while ([[NSDate date] timeIntervalSinceDate:waitStartTime] < timeout) {
-		[self updateStatusWithToPingCamera:YES];
+		[self updateStatusWithToKnockOnCamera:YES];
 		if (self.networkStatus == ReachableViaWiFi && self.cameraResponded) {
 			connected = YES;
 			break;
@@ -202,7 +204,7 @@ NSString *const WifiStatusChangedNotification = @"WifiStatusChangedNotification"
 	while ([[NSDate date] timeIntervalSinceDate:waitStartTime] < timeout) {
 		// MARK: カメラの電源オフ中にCGIコマンドを送信するとカメラが電源オフにならないようです。
 		// ここでは、カメラへCGIコマンドを送らないように電源オフ(Wi-Fi切断)を待つようにしています。
-		[self updateStatusWithToPingCamera:NO];
+		[self updateStatusWithToKnockOnCamera:NO];
 		if (self.networkStatus == NotReachable || !self.cameraResponded) {
 			disconnected = YES;
 			break;
@@ -232,14 +234,15 @@ NSString *const WifiStatusChangedNotification = @"WifiStatusChangedNotification"
 
 	__weak WifiConnector *weakSelf = self;
 	dispatch_async(weakSelf.reachabilityQueue, ^{
-		[weakSelf updateStatusWithToPingCamera:YES];
+		[weakSelf updateStatusWithToKnockOnCamera:YES];
 	});
 }
 
 #pragma mark -
 
 /// 接続状態を更新します。
-- (void)updateStatusWithToPingCamera:(BOOL)ping {
+/// 必要ならば、カメラに接続できるか否かも確かめます。
+- (void)updateStatusWithToKnockOnCamera:(BOOL)ping {
 	DEBUG_DETAIL_LOG(@"");
 	
 	NetworkStatus previousNetworkStatus = self.networkStatus;
@@ -257,7 +260,12 @@ NSString *const WifiStatusChangedNotification = @"WifiStatusChangedNotification"
 		self.cameraResponded = NO;
 		[self retreiveSSID];
 		if (ping) {
-			self.cameraResponded = [self pingCamera];
+			AppCamera *camera = GetAppCamera();
+			if (camera.connected) {
+				self.cameraResponded = YES;
+			} else {
+				self.cameraResponded = [camera canConnect:OLYCameraConnectionTypeWiFi timeout:3.0 error:nil];
+			}
 		}
 	} else {
 		// ここにはこないはず...
@@ -293,86 +301,6 @@ NSString *const WifiStatusChangedNotification = @"WifiStatusChangedNotification"
 	}
 	self.SSID = currentSSID;
 	self.BSSID = currentBSSID;
-}
-
-/// カメラにアクセスできるか否かを確かめます。
-- (BOOL)pingCamera {
-	DEBUG_DETAIL_LOG(@"");
-	
-	// 通信仕様書に従って、
-	// 単発かつその後の動作に影響の少ないと思われるCGIコマンドをカメラへ送信してそのレスポンスを確認します。
-	// 通信仕様書に沿った期待したレスポンスが返って来れば、このWi-Fiはカメラに接続していると判定します。
-
-	NSString *cameraIPAddress = @"192.168.0.10"; // カメラのIPアドレス
-	NSTimeInterval timeout = 3.0; // このタイムアウト秒数は暫定の値です。(値が短すぎると誤判断する)
-	
-	// カメラのIPアドレスへのルーティングがあるかを確認します。
-	Reachability *reachability = [Reachability reachabilityWithHostName:cameraIPAddress];
-	NSDate *waitStartTime = [NSDate date];
-	while ([reachability currentReachabilityStatus] == NotReachable &&
-		   [[NSDate date] timeIntervalSinceDate:waitStartTime] < timeout) {
-		[NSThread sleepForTimeInterval:0.05];
-	}
-	if ([reachability currentReachabilityStatus] != ReachableViaWiFi) {
-		DEBUG_LOG(@"timed out");
-		return false;
-	}
-	
-	// HTTP接続のリクエストを作成します。
-	// 送信するCGIコマンドは、接続モード取得(get_connectmode.cgi)を使います。
-	NSMutableString *url = [[NSMutableString alloc] init];
-	[url appendString:@"http://"];
-	[url appendString:cameraIPAddress];
-	[url appendString:@"/get_connectmode.cgi"];
-	NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
-	request.allowsCellularAccess = NO; // LTE/3G側には送信しないようにします。
-	request.cachePolicy = NSURLRequestReloadIgnoringCacheData;
-	request.timeoutInterval = timeout;
-	[request setValue:cameraIPAddress forHTTPHeaderField:@"Host"];
-	[request setValue:@"OlympusCameraKit" forHTTPHeaderField:@"User-Agent"];
-	NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
-	configuration.allowsCellularAccess = request.allowsCellularAccess;
-	configuration.timeoutIntervalForRequest = request.timeoutInterval;
-	configuration.timeoutIntervalForResource = request.timeoutInterval;
-	
-	// HTTP接続を試みます。
-	__block NSData *result = nil;
-	__block NSHTTPURLResponse *response = nil;
-	dispatch_semaphore_t completion = dispatch_semaphore_create(0);
-	NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
-	NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *taskResult, NSURLResponse *taskResponse, NSError *taskError) {
-		result = taskResult;
-		response = (NSHTTPURLResponse *)taskResponse;
-		dispatch_semaphore_signal(completion);
-	}];
-	[task resume];
-	dispatch_semaphore_wait(completion, DISPATCH_TIME_FOREVER);
-	[session invalidateAndCancel];
-	
-	// HTTP接続のレスポンスを確認します。
-	// 期待するCGIコマンドの応答は、XML形式でconnectmode要素のテキストがOPCになっていることです。
-	if (!result || !response) {
-		DEBUG_LOG(@"timed out");
-		return false;
-	}
-	if (response.statusCode != 200) {
-		DEBUG_LOG(@"error");
-		return false;
-	}
-	NSDictionary *fields = response.allHeaderFields;
-	NSString *contentType = fields[@"Content-Type"];
-	if (![contentType isEqualToString:@"text/xml"]) {
-		DEBUG_LOG(@"error");
-		return false;
-	}
-	NSString *resultText = [[NSString alloc] initWithData:result encoding:NSUTF8StringEncoding];
-	if ([resultText rangeOfString:@"<connectmode>OPC</connectmode>"].location == NSNotFound) {
-		DEBUG_LOG(@"error");
-		return false;
-	}
-	
-	// カメラへHTTPで接続できたようです。
-	return YES;
 }
 
 @end
